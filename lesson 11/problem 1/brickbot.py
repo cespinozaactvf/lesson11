3a3c420cec3516671a5a486ec934f87f6debb8af
cespinozaactvf <44033422+cespinozaactvf@users.noreply.github.com>
2019-01-19 11:44:18 -0500

    Add files via upload

---
+++ lesson 11/problem 1/brickbot.py
@@ -0,0 +1,29 @@
+print('_' * 50)
+
+print('Is it "Brick" or Nah Bot')
+
+print()
+
+temp = input('Temperature in Fahrenheit: ')
+
+temp = int(temp)
+
+if temp <= 45:
+	print()
+	print('It is "brick," wear a jacket.')
+	print()
+	print('Or layers.')
+	print()
+	print('Or just go outside and check.')
+	print()
+	print('Ya lazy.')
+
+else:
+	print()
+	print('It is not "brick."')
+	print()
+	print('Enjoy the heat.')
+	print()
+	print('While it lasts...')
+
+print('_' * 50)
